5985ea67867ff6d3d8f728301d1a822bed44e1e8
allissa nguyen <anguye78@masonlive.gmu.edu>
2021-10-18 14:11:54 -0400

    Completed part 1.2 - vacation

--- R/R-Worksheet1.R
+++ R/R-Worksheet1.R
@@ -11,24 +11,31 @@
 # Amazon        	500	              1600.00
 
 # 1.1	Write an R expression to store the names of the companies as a character vector
-
 # 1.2	Write an R expression to store the number of shares as a numeric vector
-
 # 1.3	Write an R expression to store the closing price as a numeric vector
-
 # 1.4	Write an R expression using your vectors and R vector arithmetic operator(s) to compute your total portfolio value (number of shares * closing price).  What is the market value of your portfolio?
 
-# 1.1 store names of companies as character vector
-companies <- c('Hewlett-Packard', 'IBM', 'Google', 'Amazon')
-num_shares <- c(1000,350,250,500) # 1.2 store number of shares as numeric vector
-closing_price <- c(20,50,1250,1600) # 1.3 store closing price as a numeric vector
+companies <- c('Hewlett-Packard', 'IBM', 'Google', 'Amazon') # stores names of companies as character vector
+num_shares <- c(1000,350,250,500) # stores number of shares as numeric vector
+closing_price <- c(20,50,1250,1600) # stores closing price as a numeric vector
 
-total_port_val <- num_shares*closing_price # 1.4 compute total portfolio value
-sum(total_port_val) 
-# market value of portfolio: 1150000
+total_port_val <- num_shares*closing_price # compute total portfolio value
+sum(total_port_val) # sum of elements
+# market value of portfolio: 1,150,000
 
 
-# Assume you want to go on a vacation at the end of the semester, and you need to book travel and hotel fares now before travel rates go up.  You decide to sell 20 shares of IBM stock and 50 shares of Hewlett-Packard stock to finance your vacation.
-# 1.5 Write an R expression to indicate the change in number of shares (hint: you may use negative numbers and add the change vector to your original vector).  
+# Assume you want to go on a vacation at the end of the semester, and you need to book travel and hotel fares now before travel rates go up.  
+# You decide to sell 20 shares of IBM stock and 50 shares of Hewlett-Packard stock to finance your vacation.
+# 1.5 Write an R expression to indicate the change in number of shares 
 # 1.6 How much is the current value of your portfolio after you make the sale?  
 # 1.7 How much money do you now have for your vacation (ignore taxes and brokerage fees for this example).
+
+to_sell <- c(50,20,0,0) # amount to sell
+port_after_sell <- num_shares-to_sell # compute shares left after sale
+total_port_val_after_sell <- port_after_sell * closing_price # computing current value
+sum(total_port_val_after_sell) # sum of elements
+# market value of portfolio after sale: 1,148,000
+
+money_for_vacay <- to_sell*closing_price # computing shares sold value
+sum(money_for_vacay) # sum of elements
+# money for vacation: 2000
