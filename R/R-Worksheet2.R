f4d9389c820c7ae404a7919d426b62dd257b7f43
allissa nguyen <anguye78@masonlive.gmu.edu>
2021-10-19 12:00:48 -0400

    Completed 3.1 & 3.2 - data frame and subsetting

--- R/R-Worksheet2.R
+++ R/R-Worksheet2.R
@@ -29,7 +29,7 @@ df1_2[c('major', 'in_state')] # filters to select major and in_state
 
 
 
-########## Problem #1: Functions #########
+########## Problem #2: Functions #########
 # Implemente the Fibonacci function.  
 # Provide output for the values from 1 to 20.
 
@@ -49,3 +49,72 @@ printf = function(s, ...){
 
 for (i in 1:20){
   printf("fib = %d, i = %d\n", fib(i),i)}
+
+
+
+########## Problem #3: Scientific Analysis and Visualization #########
+# In the lectures, we discussed various ways of rendering multiple data sets for visualization. Use your R skills to analyze the following data and prepare a scientific graph.
+# Import the Modern Applied Statistics with S+ (MASS) library into your R environment
+library( MASS )
+# From the MASS library, make a copy of the crabs data set
+crab.data = MASS::crabs
+
+# 3.1 Add a new computed column
+# Add a new column named wl.ratio to the crab.data data frame that expresses the ratio of crab carapace length to crab body length.
+# The carapace width is expressed in the CW column, and the body length is expressed in the CL column.
+# 
+# 3.2 Create subset data frames
+# Create four new data frames, using the R data frame subsetting method of your choice, that divide the data up as follows.
+# Provide R statements to extract the data into the new frames.
+# species = B, 	sex = M
+# species = B, 	sex = F
+# species = O, 	sex = M
+# species = O, 	sex = F
+# 
+# 3.3 Create a figure
+# Create a single boxplot that displays the wl.ratio for each of the data frames you just created. 
+# Include a main title, and include appropriate labels for each plot element using the names() command.
+
+library(MASS) # import MASS library
+crab.data = MASS::crabs # copied crabs data set
+
+library(dplyr) # importing dplyr to easily add new column
+crab.data.new <- crab.data %>%
+  mutate(wl.ratio = CW / CL ) # add new column called wl.ratio
+
+# creating subset data frames
+crab.data.B.M <- crab.data.new %>% 
+  filter(sp == 'B',
+         sex == 'M')
+head(crab.data.B.M)
+# sp sex index   FL  RW   CL   CW  BD wl.ratio
+# 1  B   M     1  8.1 6.7 16.1 19.0 7.0 1.180124
+# 2  B   M     2  8.8 7.7 18.1 20.8 7.4 1.149171
+# 3  B   M     3  9.2 7.8 19.0 22.4 7.7 1.178947
+
+crab.data.B.F <- crab.data.new %>% 
+  filter(sp == 'B',
+         sex == 'F')
+head(crab.data.B.F)
+# sp sex index  FL  RW   CL   CW  BD wl.ratio
+# 1  B   F     1 7.2 6.5 14.7 17.1 6.1 1.163265
+# 2  B   F     2 9.0 8.5 19.3 22.7 7.7 1.176166
+# 3  B   F     3 9.1 8.1 18.5 21.6 7.7 1.167568
+
+crab.data.O.M <- crab.data.new %>% 
+  filter(sp == 'O',
+         sex == 'M')
+head(crab.data.O.M)
+# sp sex index   FL  RW   CL   CW   BD wl.ratio
+# 1  O   M     1  9.1 6.9 16.7 18.6  7.4 1.113772
+# 2  O   M     2 10.2 8.2 20.2 22.2  9.0 1.099010
+# 3  O   M     3 10.7 8.6 20.7 22.7  9.2 1.096618
+
+crab.data.O.F <- crab.data.new %>% 
+  filter(sp == 'O',
+         sex == 'F')
+head(crab.data.O.F)
+# sp sex index   FL   RW   CL   CW   BD wl.ratio
+# 1  O   F     1 10.7  9.7 21.4 24.0  9.8 1.121495
+# 2  O   F     2 11.4  9.2 21.7 24.1  9.7 1.110599
+# 3  O   F     3 12.5 10.0 24.1 27.0 10.9 1.120332
